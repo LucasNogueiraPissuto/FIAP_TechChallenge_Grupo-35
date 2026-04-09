47ce5b0f22d01b860d4b4e68df4f88ea91f0cafc
Matheus Rocha <matheus.rocha@scicrop.com>
2026-04-09 15:07:57 -0300

    feat: entities, dockerfile, docker-compose, database migrations

---
+++ src/main/java/com/tech_challange/grupo35/entity/RestaurantOwnerEntity.java
@@ -0,0 +1,18 @@
+package com.tech_challange.grupo35.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "restaurant_owners")
+public class RestaurantOwnerEntity extends UserEntity {
+
+    @Column(name = "cnpj", nullable = false, unique = true)
+    private String cnpj;
+
+}
